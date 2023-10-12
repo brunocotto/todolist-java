2d48665d287d6cc19af57f024dfd84ef3b547dc2
brunocotto <bruno.ottoo@gmail.com>
2023-10-12 00:41:37 -0300

    Implementando segurança nos dados dos usuários

--- src/main/java/br/com/brunocotto/todolist/user/IUserRepository.java
+++ src/main/java/br/com/brunocotto/todolist/user/IUserRepository.java
@@ -5,5 +5,5 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUserRepository extends JpaRepository<UserModel, UUID> {
-  UserModel findbyUsername(String username);
+  UserModel findByUsername(String username);
 }
--- src/main/java/br/com/brunocotto/todolist/user/UserController.java
+++ src/main/java/br/com/brunocotto/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -17,11 +19,15 @@ public class UserController {
   
   @PostMapping("/")
   public ResponseEntity create(@RequestBody UserModel userModel) {
-    var user = this.userRepository.findbyUsername(userModel.getUsername());
+    var user = this.userRepository.findByUsername(userModel.getUsername());
 
     if (user != null) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
     }
+
+    var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashed);
 
     var userCreated = this.userRepository.saveAndFlush(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
